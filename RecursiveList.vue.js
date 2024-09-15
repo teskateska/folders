43679ced4d2ea80ffa62d79046bb67d5c03ae208
Harris Ebdon <101606043@student.swin.edu.au>
2024-09-15 13:06:42 +1000

    prog

--- RecursiveList.vue.js
+++ RecursiveList.vue.js
@@ -1,49 +1,52 @@
 Vue.component("RecursiveList", {
   template: `
       <div>
-        <template
-          v-for="(item, i) in rootItems"
-        >
           <v-list-item
-            v-for="(item, i) in rootItems"
             :class="padding"
             :key="item.id"
+            :value="item.id"
           >
-              <v-list-item-icon>
-                <v-icon @click.prevent="handleExpand(i)">
-                  {{item.isExpanded ?  'mdi-menu-down' : 'mdi-menu-right'}}
-                </v-icon>
+            <v-list-item-icon>
+              <v-icon @click.prevent="handleExpand(item.id)">
+                {{item.isExpanded ?  'mdi-menu-down' : 'mdi-menu-right'}}
+              </v-icon>
               <v-icon class="pl-4">mdi-folder</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title>{{item.name}}</v-list-item-title>
-              </v-list-item-content>
+              <v-list-item-title>
+                {{item.name}}
+              </v-list-item-title>
+            </v-list-item-content>
             <v-list-item-content>
-              <v-list-item-subtitle class="text-right">{{item.category}}</v-list-item-subtitle>
+              <v-list-item-subtitle class="text-right">
+                {{item.category}}
+              </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
-          <recursive-list
-            :v-if="hasChildren(item.id)"
-            :items="items"
-            :parentId="item.id"
-            :paddingScalar="paddingScalar + 1"
+          <template 
+            v-if="hasChildren && item.isExpanded"
           >
-          </recursive-list>
+            <recursive-list 
+              v-for="child in children"
+              :key="child.id"
+              :item="child"
+              :items="items"
+              :paddingScalar="paddingScalar + 1"
+              @expand="handleExpand"
+            >
+            </recursive-list>
+          </template>
         </template>
       </div>
   `,
   props: {
     item: {
       type: Object,
-      required,
+      required: true,
     },
     items: {
       type: Array,
       default: [],
-    },
-    parentId: {
-      type: Number,
-      default: null,
     },
     paddingScalar: {
       type: Number,
@@ -57,15 +60,15 @@ Vue.component("RecursiveList", {
   },
   methods: {
     handleExpand(id) {
-      this.items[id].isExpanded = !this.items[id].isExpanded;
-    },
-    hasChildren(id) {
-      return this.items.filter((item) => item.parentId == id).length;
+      this.$emit("expand", id);
     },
   },
   computed: {
-    rootItems() {
-      return this.items.filter((items) => items.parentId == this.parentId);
+    children() {
+      return this.items.filter((item) => item.parentId === this.item.id);
+    },
+    hasChildren() {
+      return this.children.length;
     },
     padding() {
       return "pl-" + this.paddingScalar * 4;
--- parent.vue.js
+++ parent.vue.js
@@ -6,7 +6,11 @@ Vue.component("parent", {
                   v-model="selectedItem"
                 >
                   <recursive-list
-                      :items="list"
+                    v-for="item in rootItems"
+                    :key="item.id"
+                    :item="item"
+                    :items="items"
+                    @expand="handleExpand"
                   ></recursive-list>
                 </v-list-item-group>
             </v-list>
@@ -15,7 +19,7 @@ Vue.component("parent", {
   data() {
     return {
       selectedItem: 1,
-      list: [
+      items: [
         {
           id: 1,
           name: "parent",
@@ -37,7 +41,30 @@ Vue.component("parent", {
           isExpanded: false,
           parentId: 1,
         },
+        {
+          id: 4,
+          name: "child of child!",
+          category: "apple",
+          isExpanded: false,
+          parentId: 3,
+        },
       ],
     };
+  },
+  watch: {
+    selectedItem() {
+      console.log(this.selectedItem);
+    },
+  },
+  computed: {
+    rootItems() {
+      return this.items.filter((item) => item.parentId === null);
+    },
+  },
+  methods: {
+    handleExpand(id) {
+      const target = this.items.find((item) => item.id === id);
+      target.isExpanded = !target.isExpanded;
+    },
   },
 });
