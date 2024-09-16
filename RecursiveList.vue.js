91cf0c1d5fdd45f42becaa821faef59da6440d59
Harris Ebdon <101606043@student.swin.edu.au>
2024-09-16 17:05:03 +1000

    prog

--- RecursiveList.vue.js
+++ RecursiveList.vue.js
@@ -1,16 +1,18 @@
 Vue.component("RecursiveList", {
-  template: `
+    template: `
       <div>
           <v-list-item
             :class="padding"
             :key="item.id"
             :value="item.id"
           >
-            <v-list-item-icon>
-              <v-icon @click.prevent="handleExpand(item.id)">
+            <v-list-item-icon @click.prevent="handleExpand(item.id)"
+                class="mr-4"
+            >
+              <v-icon>
                 {{item.isExpanded ?  'mdi-menu-down' : 'mdi-menu-right'}}
               </v-icon>
-              <v-icon class="pl-4">
+              <v-icon class="pl-2">
                 {{item.isExpanded ? 'mdi-folder-open' : 'mdi-folder'}}
               </v-icon>
             </v-list-item-icon>
@@ -20,73 +22,67 @@ Vue.component("RecursiveList", {
               </v-list-item-title>
             </v-list-item-content>
             <v-list-item-content>
-              <v-list-item-subtitle class="text-right">
+              <v-list-item-title class="text-right">
                 {{item.category}}
-              </v-list-item-subtitle>
+              </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
+          <v-divider></v-divider>
           <v-expand-transition>
             <div 
               v-show="hasChildren && item.isExpanded"
             >
-              <recursive-list 
-                v-for="child in children"
-                :key="child.id"
-                :item="child"
-                :items="items"
-                :paddingScalar="paddingScalar + 1"
-                @expand="handleExpand"
-              >
+                <recursive-list 
+                    v-for="child in children"
+                    :key="child.id"
+                    :item="child"
+                    :items="items"
+                    :paddingScalar="paddingScalar + 1"
+                    @expand="handleExpand"
+                >
               </recursive-list>
             </div>
           </v-expand-transition>
-        </template>
       </div>
   `,
-  props: {
-    item: {
-      type: Object,
-      required: true,
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+        items: {
+            type: Array,
+            default: [],
+        },
+        paddingScalar: {
+            type: Number,
+            default: 1,
+        },
     },
-    items: {
-      type: Array,
-      default: [],
+    methods: {
+        handleExpand(id) {
+            this.$emit("expand", id);
+        },
     },
-    parentId: {
-      type: Number,
-      default: null,
+    computed: {
+        children() {
+            if (this.item.isOptionGroup) {
+                return this.items.filter(
+                    (item) =>
+                        item.optionGroupId === this.item.id &&
+                        item.parentId === null
+                );
+            } else {
+                return this.items.filter(
+                    (item) => item.parentId === this.item.id
+                );
+            }
+        },
+        hasChildren() {
+            return this.children.length;
+        },
+        padding() {
+            return "pl-" + this.paddingScalar * 4;
+        },
     },
-    paddingScalar: {
-      type: Number,
-      default: 1,
-    },
-  },
-  data() {
-    return {
-      selectedItem: 1,
-    };
-  },
-  methods: {
-    handleExpand(id) {
-      this.$emit("expand", id);
-    },
-  },
-  computed: {
-    children() {
-      if (this.item.isOptionGroup) {
-        return this.items.filter(
-          (item) =>
-            item.optionGroupId === this.item.id && item.parentId === null
-        );
-      } else {
-        return this.items.filter((item) => item.parentId === this.item.id);
-      }
-    },
-    hasChildren() {
-      return this.children.length;
-    },
-    padding() {
-      return "pl-" + this.paddingScalar * 4;
-    },
-  },
 });
--- parent.vue.js
+++ parent.vue.js
@@ -1,373 +1,181 @@
 Vue.component("parent", {
-  template: `
-        <v-card class="d-flex flex-column" style="height: 100%">
-          <v-list>
-            <v-list-item-group
-              v-model="selectedItem"
-            >
-              <recursive-list
-                v-for="item in rootItems"
-                :key="item.id"
-                :item="item"
-                :items="combinedItems"
-                :parentId="parentId"
-                @expand="handleExpand"
-              ></recursive-list>
-            </v-list-item-group>
-          </v-list>
-          <div class="px-4 py-4 mt-auto">
-            <v-btn 
-              block color="success" 
-              class="btnsubmit afBtn afBtn__fill af-success" 
-              :disabled="false" 
-              @click.prevent="handleSubmit()"
-              >
-              {{ parentId === selectedItem ?
-                  "REMOVE FROM ASSEMBLY" : "MOVE TO ASSEMBLY" }}
-            </v-btn>
-          </div>
-        </v-card>
+    template: `
+        <v-sheet class="d-flex flex-column">
+            <v-list>
+                <v-divider></v-divider>
+                <v-list-item class="justify-"> 
+                    <v-list-item-content class="ml-16 font-weight-bold">
+                        <v-list-item-subtitle>
+                            Description
+                        </v-list-item-subtitle>
+                    </v-list-item-content>
+                    <v-list-item-content 
+                        class="text-right mr-4 font-weight-bold"
+                    >
+                        <v-list-item-subtitle>
+                            Type
+                        </v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item-group v-model="selectedMovingTo">
+                    <recursive-list
+                        v-for="item in rootItems"
+                        :key="item.id"
+                        :item="item"
+                        :items="combinedItems"
+                        @expand="handleExpand"
+                    ></recursive-list>
+                </v-list-item-group>
+            </v-list>
+            <div class="px-4 py-4" style="width: 100%; position: fixed; bottom: 0;">
+                <v-btn
+                    block
+                    color="success" 
+                    class="btnsubmit afBtn afBtn__fill af-success" 
+                    :disabled="false" 
+                    @click.prevent="handleSubmit()"
+                >
+                    {{ buttonText }}
+                </v-btn>
+            </div>
+        </v-sheet>
         `,
-  data() {
-    return {
-      parentId: 4,
-      selectedItem: 4,
-      combinedItems: [],
-      optionGroups: [
-        //{
-        //          optionGroupId: 2222,
-        //          name: "option grp",
-        //          category: "option group",
-        //},
-      ],
-      items: [
-        {
-          id: 1,
-          name: "Roman Empire",
-          category: "assembly",
-          isExpanded: true,
-          optionGroupId: null,
-          parentId: null,
-        },
-        {
-          id: 2,
-          name: "Foundation",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 1,
-        },
-        {
-          id: 3,
-          name: "Founding of Rome",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 2,
-        },
-        {
-          id: 4,
-          name: "753 BCE - Romulus and Remus",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 3,
-        },
-        {
-          id: 5,
-          name: "Roman Kingdom",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 1,
-        },
-        {
-          id: 6,
-          name: "753 BCE - 509 BCE",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 5,
-        },
-        {
-          id: 7,
-          name: "Roman Republic",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 1,
-        },
-        {
-          id: 8,
-          name: "509 BCE - 27 BCE",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 7,
-        },
-        {
-          id: 9,
-          name: "Punic Wars",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 7,
-        },
-        {
-          id: 10,
-          name: "First Punic War (264-241 BCE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 9,
-        },
-        {
-          id: 11,
-          name: "Second Punic War (218-201 BCE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 9,
-        },
-        {
-          id: 12,
-          name: "Third Punic War (149-146 BCE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 9,
-        },
-        {
-          id: 13,
-          name: "Roman Empire",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 1,
-        },
-        {
-          id: 14,
-          name: "27 BCE - 476 CE (Western)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 15,
-          name: "27 BCE - 1453 CE (Eastern)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 16,
-          name: "Julio-Claudian Dynasty",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 17,
-          name: "Augustus (27 BCE - 14 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 16,
-        },
-        {
-          id: 18,
-          name: "Tiberius (14-37 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 16,
-        },
-        {
-          id: 19,
-          name: "Caligula (37-41 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 16,
-        },
-        {
-          id: 20,
-          name: "Claudius (41-54 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 16,
-        },
-        {
-          id: 21,
-          name: "Nero (54-68 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 16,
-        },
-        {
-          id: 22,
-          name: "Five Good Emperors",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 23,
-          name: "Nerva (96-98 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 22,
-        },
-        {
-          id: 24,
-          name: "Trajan (98-117 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 22,
-        },
-        {
-          id: 25,
-          name: "Hadrian (117-138 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 22,
-        },
-        {
-          id: 26,
-          name: "Antoninus Pius (138-161 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 22,
-        },
-        {
-          id: 27,
-          name: "Marcus Aurelius (161-180 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 22,
-        },
-        {
-          id: 28,
-          name: "Crisis of the Third Century",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 29,
-          name: "235-284 CE",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 28,
-        },
-        {
-          id: 30,
-          name: "Diocletian and Tetrarchy",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 31,
-          name: "284-305 CE",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 30,
-        },
-        {
-          id: 32,
-          name: "Constantine and Christianity",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 33,
-          name: "306-337 CE",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 32,
-        },
-        {
-          id: 34,
-          name: "Edict of Milan (313 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 32,
-        },
-        {
-          id: 35,
-          name: "Fall of Western Roman Empire",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 36,
-          name: "476 CE - Odoacer deposes Romulus Augustulus",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 35,
-        },
-        {
-          id: 37,
-          name: "Eastern Roman Empire (Byzantine)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 13,
-        },
-        {
-          id: 38,
-          name: "Justinian I (527-565 CE)",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 37,
-        },
-        {
-          id: 39,
-          name: "Fall of Constantinople",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 37,
-        },
-        {
-          id: 40,
-          name: "1453 CE - Ottoman conquest",
-          category: "assembly",
-          optionGroupId: null,
-          parentId: 39,
-        },
-      ],
-    };
-  },
-  watch: {
-    selectedItem() {
-      console.log(this.selectedItem);
+    data() {
+        return {
+            selectedMovingFrom: 0,
+            selectedMovingTo: 0,
+            combinedItems: [],
+            optionGroups: [
+                {
+                    optionGroupId: 2222,
+                    name: "Option Group",
+                    category: "Option Group",
+                },
+            ],
+            items: [
+                {
+                    id: 1,
+                    name: "fruit",
+                    category: "assembly",
+                    isExpanded: true,
+                    optionGroupId: null,
+                    parentId: null,
+                },
+                {
+                    id: 1333,
+                    name: "move fruit",
+                    category: "assembly",
+                    isExpanded: true,
+                    optionGroupId: null,
+                    parentId: null,
+                },
+                {
+                    id: 2,
+                    name: "animals",
+                    category: "assembly",
+                    optionGroupId: null,
+                    parentId: 1,
+                },
+                {
+                    id: 3,
+                    name: "countries",
+                    category: "assembly",
+                    optionGroupId: null,
+                    parentId: 2,
+                },
+            ],
+        };
     },
-  },
-  mounted() {
-    this.combinedItems = [
-      ...this.items.map((item) => ({
-        ...item,
-        isExpanded: false,
-        isOptionGroup: false,
-      })),
-      ...this.optionGroups.map((optionGroup) => ({
-        id: optionGroup.optionGroupId,
-        name: optionGroup.name,
-        category: optionGroup.category,
-        isExpanded: true,
-        optionGroupId: null,
-        isOptionGroup: true,
-        parentId: null,
-      })),
-    ];
-  },
-  computed: {
-    rootItems() {
-      return this.combinedItems.filter(
-        (item) => item.parentId === null && item.optionGroupId === null
-      );
+    watch: {
+        selectedItem() {
+            console.log(this.selectedItem);
+        },
     },
-  },
-  methods: {
-    handleExpand(id) {
-      const target = this.combinedItems.find((item) => item.id === id);
-      target.isExpanded = !target.isExpanded;
+    mounted() {
+        this.combinedItems = [
+            ...this.items.map((item) => ({
+                ...item,
+                isExpanded: true,
+                isOptionGroup: false,
+            })),
+            ...this.optionGroups.map((optionGroup) => ({
+                id: optionGroup.optionGroupId,
+                name: optionGroup.name,
+                category: optionGroup.category,
+                isExpanded: true,
+                optionGroupId: null,
+                isOptionGroup: true,
+                parentId: null,
+            })),
+        ];
     },
-    handleSubmit() {
-      const target = this.combinedItems.find(
-        (item) => item.id === this.selectedItem
-      );
-      console.log("moved to: ", target.name);
-      console.log(
-        target.isOptionGroup ? "its an option group" : "its an assembly"
-      );
+    computed: {
+        isMovingOut() {
+            return this.selectedMovingFrom === this.selectedMovingTo;
+        },
+        rootItems() {
+            return this.combinedItems.filter(
+                (item) => item.parentId === null && item.optionGroupId === null
+            );
+        },
+        buttonText() {
+            if (this.selectedMovingFrom) {
+                if (this.isMovingOut) {
+                    return "REMOVE ITEM";
+                } else {
+                    return "MOVE ITEM TO";
+                }
+            } else {
+                return "SELECT AN ITEM";
+            }
+        },
     },
-  },
+    methods: {
+        handleExpand(id) {
+            const target = this.combinedItems.find((item) => item.id === id);
+            target.isExpanded = !target.isExpanded;
+        },
+        handleSubmit() {
+            if (!this.selectedMovingFrom) {
+                this.selectedMovingFrom = this.selectedMovingTo;
+                this.selectedMovingTo = null;
+                return;
+            }
+
+            const from = this.combinedItems.find(
+                (item) => item.id === this.selectedMovingFrom
+            );
+            if (from.isOptionGroup) {
+                alert("Wtf you cannot move an option group.");
+                this.selectedMovingFrom = null;
+                return;
+            }
+            console.log("moving ", from.id);
+
+            const to = this.combinedItems.find(
+                (item) => item.id === this.selectedMovingTo
+            );
+            console.log("to ", to.id);
+
+            if (to.isOptionGroup) {
+                if (this.isMovingOut) {
+                    from.optionGroupId = null;
+                } else {
+                    from.optionGroupId = to.id;
+                }
+            } else {
+                if (this.isMovingOut) {
+                    from.parentId = null;
+                } else {
+                    from.parentId = to.id;
+                }
+            }
+
+            console.log("Here the updated object", from);
+
+            this.selectedMovingFrom = null;
+            this.selectedMovingTo = null;
+        },
+    },
 });
